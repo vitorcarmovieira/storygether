145bd8f05401dbd86ff528449e22d63bc86b956d
Vinícius Cerqueira Silva <vini.cerqueira12@gmail.com>
2015-06-30 00:36:40 -0400

    login view

--- StoryGether/LoginViewController.swift
+++ StoryGether/LoginViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UIScrollViewDelegate {
     @IBOutlet weak var svTutorial: UIScrollView!
     
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbSub: UILabel!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var btLogin: UIButton!
     var images: NSArray!
+    var titles: NSArray!
+    var subs: NSArray!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.images = NSArray(array: [UIImage(named: "storygether_telainicial1")!,UIImage(named: "storygether_telainicial2")!,UIImage(named: "storygether_telainicial3")!]);
+        self.titles = NSArray(array: ["Make stories","Begin a story","Have fun"])
+        self.subs = NSArray(array: ["with your friends","or continue one","and be creative"])
         
-        
+        self.svTutorial.delegate=self
         
         for (var i: Int = 0; i < self.images.count; i++) {
             
@@ -47,5 +53,14 @@ class LoginViewController: UIViewController {
         self.svTutorial.contentSize =
             CGSizeMake(self.svTutorial.frame.size.width * CGFloat(self.images.count), self.svTutorial.frame.size.height)
         
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var pageWidth: CGFloat = self.svTutorial.frame.size.width;
+        
+        var page: Int  = Int(floor((self.svTutorial.contentOffset.x - pageWidth / 2.0) / pageWidth)) + 1;
+        self.lbTitle.text = self.titles[page] as? String
+        self.lbSub.text = self.subs[page] as? String
+        self.pageControl.currentPage = page;
     }
 }
